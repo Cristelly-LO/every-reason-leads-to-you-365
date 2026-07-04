b0932a814d2aa0d749ff717981a258647d8e6050
Cristelly-LO <cristelaltarejos@gmail.com>
2026-07-04 22:08:05 +0800

    Refactor DOMContentLoaded event listener syntax

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
+
 document.body.classList.add("loaded");
+
+const loader = document.getElementById("loader");
+
+if (loader) {
+
+setTimeout(function () {
+
+loader.style.opacity = "0";
+
+setTimeout(function () {
+loader.remove();
+}, 1000);
+
+}, 2500);
+
+}
+
 });
